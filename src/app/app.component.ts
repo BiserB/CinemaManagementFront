a58671c8384413363ff360efbae3391bbdbe0b8c
BiserBorisov <butoff@abv.bg>
2019-10-07 23:58:01 +0300

    Helper Frontend for Cinema Management Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cinemaList: { Id:number, Name: string, Address: string}[] = [];
+  cinemaDetails: Object = {};
+
+  unstartedProjections: any[] = [];
+  projectionDetails: Object = {};
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  onGetCinema(id: number){
+
+    this.http.get("http://localhost:50715/api/cinema/getCinema/" + id)
+      .subscribe(res => {
+        this.cinemaDetails = res;
+      });
+  }
+
+  onGetCinemaList(){
+
+    this.http.get("http://localhost:50715/api/cinema/getCinemaList")
+      .subscribe((res: { Id:number, Name: string, Address: string}[]) => {
+        this.cinemaList = res;
+      });
+  }
+
+  onGetUnstarted(){
+
+    this.http.get("http://localhost:50715/api/projection/getUnstarted")
+      .subscribe((res: any[]) => {
+        this.unstartedProjections = res;
+      });
+  }
+
+  onGetProjection(id: number){
+
+    this.http.get("http://localhost:50715/api/projection/get/" + id)
+      .subscribe(res => {
+        this.projectionDetails = res;
+      });
+  }
+}
