de3758ff9fe3a7c2b88977200c78b4349c4d1153
BiserBorisov <butoff@abv.bg>
2019-10-10 03:07:02 +0300

    Add methods to test functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,17 @@ export class AppComponent {
   unstartedProjections: any[] = [];
   projectionDetails: Object = {};
 
-  constructor(private http: HttpClient) {
+  reservationId: number = 0;
+  roomId: number = 0;
+  movieId: number = 0;
+  startDate: Date = new Date();
 
+  row: number = 0;
+  column: number = 0;
+
+  constructor(private http: HttpClient) {
+    let dateString = '2019-10-29T02:20:00' 
+    this.startDate = new Date(dateString);
   }
 
   onGetCinema(id: number){
@@ -53,7 +62,25 @@ export class AppComponent {
   inspect(){
     this.http.get("http://localhost:50715/api/projection/inspect")
       .subscribe(res => {
-        console.log("Ispected", res);
+        console.log("Ispected..", res);
+      });
+  }
+
+  onCancelReservation(){
+
+    this.http.post(`http://localhost:50715/api/reservation/cancel/${this.reservationId}`, null )
+      .subscribe(res => {
+        console.log("Cancel..", res);
+      });
+  }
+
+  onCreateProjection(){
+
+    const model = { roomId: this.roomId, movieId: this.movieId, startDate: this.startDate}
+
+    this.http.post(`http://localhost:50715/api/projection/create/`, model )
+      .subscribe(res => {
+        console.log("Create..", res);
       });
   }
 }
